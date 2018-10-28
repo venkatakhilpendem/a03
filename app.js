c3f612902df4c246fec4f3a0e08f1c9260a54238
venkatakhilpendem <venkatakhil.pendem@gmail.com>
2018-10-28 16:24:46 -0500

    UPDATE

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT||8081
 // ADD THESE COMMENTS AND IMPLEMENTATION HERE
 // 1 set up the view engine
 // 1 set up the view engine
-app.set("views", path.resolve(__dirname, "views")) // path to views
+app.set("views", path.resolve(__dirname, "Views")) // path to views
 app.set("view engine", "ejs") // specify our view
 // 2 include public assets and use bodyParser
 app.use(express.static(__dirname + '/public'));
@@ -25,7 +25,7 @@ app.use(logger('combined', { stream: accessLogStream }));
 // 4 handle valid GET requests
 app.get("/", function (req, res) {
  //res.sendFile(path.join(__dirname + '/assets/index.html'))
- res.render("index.ejs")
+ res.render("Index.ejs")
 })
 app.get("/Index", function (req, res) {
   res.render("Index.ejs")
